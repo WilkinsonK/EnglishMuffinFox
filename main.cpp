6c7af9150c4e418efe7a874a11aa0e3b652326b7
Keenan W. Wilkinson <keenanwilkinson@outlook.com>
2023-01-01 11:21:45 -0600

    changes to play with fds

--- main.cpp
+++ main.cpp
@@ -1,57 +1,81 @@
 #include <fstream>
 #include <iostream>
 
-#include <argparse/argparse.hpp>
+
+struct file_object {
+    public:
+        file_object(std::string path)
+        {
+            this->path = path;
+            this->stream = nullptr;
+        }
+
+        bool is_closed()
+        {
+            return !(this->stream != nullptr && this->stream->is_open());
+        }
+
+        void open(std::ios_base::openmode mode)
+        {
+            std::shared_ptr<std::fstream> s(new std::fstream());
+            this->stream = s;
+            this->stream->open(this->path, mode);
+        }
+
+        void close()
+        {
+            if (!is_closed())
+                this->stream->close();
+            this->stream = nullptr;
+        }
+
+        void operator<<(const char* message)
+        {
+            open(std::ios::out);
+            if (is_closed())
+            {
+                std::cerr << "error: could not open file '"
+                          << this->path
+                          << "' to write"
+                          << std::endl;
+                std::exit(1);
+            }
+
+            *(this->stream) << message;
+            close();
+        }
+
+        void operator>>(std::string buffer)
+        {
+            open(std::ios::in);
+            if (is_closed())
+            {
+                std::cerr << "error: could not open file '"
+                    << this->path
+                    << "' to read"
+                    << std::endl;
+                std::exit(1);
+            }
+
+
+            *(this->stream) >> buffer;
+            close();
+        }
+
+    private:
+        std::string path;
+        std::shared_ptr<std::fstream> stream;
+};
+
 
 int main(int argc, const char* const* argv)
 {
-    // Initialize our root parser.
-    // Apply any details that might be helpful for the user.
-    argparse::ArgumentParser rootProgram("emf");
-    rootProgram.add_description("example cli program");
+    file_object file("test.txt");
+    file << "Some text.\nSome text.\nSome text.\n";
 
-    // Setup any options/arguments used in this cli program.
-    rootProgram.add_argument("file");
-
-    // Parse given arguments (if any) raising an error for
-    // the user if something goes wrong.
-    try {
-        rootProgram.parse_args(argc, argv);
-    } catch (std::runtime_error& err) {
-        std::cerr << "error: " << err.what() << std::endl;
-        return -1;
-    }
-
-    std::fstream sfile;
-    std::string file(rootProgram.get<std::string>("file"));
-
-    sfile.open(file, std::ios::out);
-    sfile << "Some text.\nSome text.\nSome text.\n";
-    sfile.close();
-
-    char readBuffer[10 * 1024];
-    std::vector<char*> data;
-
-    sfile.open(file, std::ios::in);
-    while (sfile >> std::noskipws >> readBuffer)
-    {
-        uint32_t readBytes = 0;
-        for (auto ch : readBuffer)
-        {
-            std::cout << ch;
-            readBytes++;
-        }
-        data.push_back(readBuffer);
-        std::cout << "# of Bytes Read: " << readBytes << std::endl;
-        std::cout << readBuffer;
-    }
-    sfile.close();
-
-    for (auto i : data)
-    {
-        std::cout << i;
-    }
-    std::cout << std::endl;
+    std::string buffer;
+    file >> buffer;
+    std::cout << "from test: " << buffer << std::endl;
 
     return 0;
 }
